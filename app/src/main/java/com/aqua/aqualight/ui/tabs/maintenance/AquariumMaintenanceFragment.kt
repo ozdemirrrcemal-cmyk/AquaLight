e1a55b47e10838edec20aca3b3d24a7f675e24da
Test User <test@example.com>
2025-12-07 15:19:49 +0000

    update: new ui fixes

---
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/maintenance/AquariumMaintenanceFragment.kt
@@ -0,0 +1,8 @@
+package com.aqua.aqualight.ui.tabs.maintenance
+
+import androidx.fragment.app.Fragment
+import com.aqua.aqualight.R
+
+class AquariumMaintenanceFragment : Fragment(R.layout.fragment_aquarium_maintenance) {
+    // Akvaryum bakım bilgileri logic'i buraya
+}
