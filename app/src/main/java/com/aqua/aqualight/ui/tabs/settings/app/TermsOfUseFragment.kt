c2889a5c951b997ef78b224afbe27815073b7a9b
Test User <test@example.com>
2026-05-12 12:36:46 +0000

    fix: versiyoncode

--- app/src/main/java/com/aqua/aqualight/ui/tabs/settings/app/TermsOfUseFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/settings/app/TermsOfUseFragment.kt
@@ -2,6 +2,7 @@ package com.aqua.aqualight.ui.tabs.settings.app
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aqua.aqualight.R
@@ -12,17 +13,44 @@ class TermsOfUseFragment : Fragment(R.layout.fragment_terms_of_use) {
     private var _binding: FragmentTermsOfUseBinding? = null
     private val binding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
         super.onViewCreated(view, savedInstanceState)
+
         _binding = FragmentTermsOfUseBinding.bind(view)
 
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+
+        binding.webViewTerms.apply {
+
+            webViewClient = WebViewClient()
+
+            settings.javaScriptEnabled = false
+            settings.domStorageEnabled = false
+
+            settings.allowFileAccess = false
+            settings.allowContentAccess = false
+
+            loadUrl("file:///android_asset/terms_of_use_en.html")
+        }
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
+
+        binding.webViewTerms.apply {
+            stopLoading()
+            loadUrl("about:blank")
+            clearHistory()
+            removeAllViews()
+            destroy()
+        }
+
         _binding = null
+
+        super.onDestroyView()
     }
 }
