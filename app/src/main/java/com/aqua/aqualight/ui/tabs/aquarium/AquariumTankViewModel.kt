ee857ec0ac0325f0e7fea8b1d221f023100e9047
Test User <test@example.com>
2026-05-21 02:59:17 +0000

    fix: versiyoncode

--- app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/AquariumTankViewModel.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/AquariumTankViewModel.kt
@@ -3,12 +3,10 @@ package com.aqua.aqualight.ui.tabs.aquarium
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import com.aqua.aqualight.data.tanks.AquariumTankDataStoreManager
 import com.aqua.aqualight.ui.tabs.aquarium.create.TankDraft
 import com.aqua.aqualight.ui.tabs.aquarium.model.SavedAquariumTank
-import kotlinx.coroutines.launch
 
 class AquariumTankViewModel(
     application: Application
@@ -18,16 +16,8 @@ class AquariumTankViewModel(
         application.applicationContext
     )
 
-    private val _tanks = MutableLiveData<List<SavedAquariumTank>>(emptyList())
-    val tanks: LiveData<List<SavedAquariumTank>> = _tanks
-
-    init {
-        viewModelScope.launch {
-            tankDataStoreManager.tanksFlow.collect { tanks ->
-                _tanks.value = tanks
-            }
-        }
-    }
+    val tanks: LiveData<List<SavedAquariumTank>> =
+        tankDataStoreManager.tanksFlow.asLiveData()
 
     suspend fun addTankFromDraft(
         draft: TankDraft
