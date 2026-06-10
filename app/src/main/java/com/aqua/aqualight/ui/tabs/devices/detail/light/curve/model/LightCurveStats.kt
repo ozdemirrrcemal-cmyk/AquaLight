82c45bc385f242e4493a57ef0494a0d20021285e
Test User <test@example.com>
2026-06-10 12:03:52 +0000

    update: new ui fixes

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/light/curve/model/LightCurveStats.kt
+++
@@ -1,7 +0,0 @@
-package com.aqua.aqualight.ui.tabs.devices.detail.light.curve.model
-
-data class LightCurveStats(
-    val outputPercent: Int,
-    val estimatedPowerWatts: Double,
-    val durationHours: Double
-)
