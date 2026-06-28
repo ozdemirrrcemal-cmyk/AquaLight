c1944d41a4eb48e0c94357355c093669b554505f
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-06-28 17:51:04 +0300

    Add Wi-Fi provisioning draft factory

---
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/add/DeviceWifiProvisioningDraftFactory.kt
@@ -0,0 +1,42 @@
+package com.aqua.aqualight.ui.tabs.devices.add
+
+import com.aqua.aqualight.data.devices.provisioning.model.AqlProvisioningDraft
+import com.aqua.aqualight.data.devices.provisioning.model.AqlWifiCredentials
+import com.aqua.aqualight.data.devices.provisioning.store.AqlProvisioningDraftStore
+import java.util.TimeZone
+
+object DeviceWifiProvisioningDraftFactory {
+
+    fun create(
+        args: DeviceWifiProvisioningFragmentArgs,
+        ssid: String,
+        networkKey: String
+    ): Result<AqlProvisioningDraft> {
+        return runCatching {
+            val deviceTimeZone = TimeZone.getDefault()
+            val utcOffsetMinutes = deviceTimeZone.getOffset(System.currentTimeMillis()) / MILLIS_PER_MINUTE
+            val timeZoneId = deviceTimeZone.id.orEmpty()
+            val credentials = AqlWifiCredentials(
+                ssid = ssid,
+                password = networkKey,
+                timezone = "$timeZoneId$TIMEZONE_OFFSET_SEPARATOR$utcOffsetMinutes",
+                utcOffsetMinutes = utcOffsetMinutes
+            )
+
+            AqlProvisioningDraftStore.create(
+                candidateId = args.candidateId,
+                bleAddress = args.bleAddress,
+                bleName = args.bleName,
+                claimCode = args.claimCode,
+                rawQrPayload = args.rawQrPayload,
+                deviceTitle = args.deviceTitle,
+                deviceSerial = args.deviceSerial,
+                deviceModel = args.deviceModel,
+                wifiCredentials = credentials
+            )
+        }
+    }
+
+    private const val MILLIS_PER_MINUTE = 60_000
+    private const val TIMEZONE_OFFSET_SEPARATOR = "|"
+}
