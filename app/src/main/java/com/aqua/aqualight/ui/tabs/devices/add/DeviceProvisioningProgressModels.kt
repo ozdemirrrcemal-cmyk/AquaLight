d41f9adafc7a834ead994cf2f4e7eef5a38d59e4
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-03 02:30:55 +0300

    Restore provisioning progress models

---
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/add/DeviceProvisioningProgressModels.kt
@@ -0,0 +1,24 @@
+package com.aqua.aqualight.ui.tabs.devices.add
+
+import com.aqua.aqualight.ui.tabs.devices.route.DeviceRoute
+
+sealed interface DeviceProvisioningProgressEvent {
+    data class OpenAddedDevice(
+        val route: DeviceRoute
+    ) : DeviceProvisioningProgressEvent
+}
+
+data class DeviceProvisioningProgressUiState(
+    val title: String = "",
+    val message: String = "",
+    val deviceName: String = "",
+    val deviceSerial: String = "",
+    val bleAddress: String = "",
+    val wifiSsid: String = "",
+    val stepOne: String = "",
+    val stepTwo: String = "",
+    val stepThree: String = "",
+    val canStart: Boolean = false,
+    val buttonText: String = "",
+    val showProgress: Boolean = false
+)
