bbf599f0900b315f0a45b53e3c28edc8005f427d
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-05 12:27:22 +0300

    Add Wi-Fi provisioning retry result contract

---
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/add/DeviceWifiProvisioningResult.kt
@@ -0,0 +1,9 @@
+package com.aqua.aqualight.ui.tabs.devices.add
+
+internal object DeviceWifiProvisioningResult {
+    const val KEY_FAILURE_MESSAGE = "device_wifi_provisioning_failure_message"
+    const val KEY_FAILURE_FIELD = "device_wifi_provisioning_failure_field"
+
+    const val FIELD_PASSWORD = "password"
+    const val FIELD_SSID = "ssid"
+}
