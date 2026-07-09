046850b7e86b2d31ad9d9637a5fc98e3052bdd6a
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-09 16:03:20 +0300

    Restore settings root during theme recreation

--- app/src/main/java/com/aqua/aqualight/ui/common/bottomsheet/ThemeBottomSheet.kt
+++ app/src/main/java/com/aqua/aqualight/ui/common/bottomsheet/ThemeBottomSheet.kt
@@ -7,6 +7,7 @@ import com.aqua.aqualight.R
 import com.aqua.aqualight.base.theme.AppThemeController
 import com.aqua.aqualight.data.user.UserPreferencesManager
 import com.aqua.aqualight.databinding.DialogThemeSelectionBinding
+import com.aqua.aqualight.ui.main.MainActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -60,8 +61,15 @@ class ThemeBottomSheet : BottomSheetDialogFragment(R.layout.dialog_theme_selecti
 
             val appContext =
                 context?.applicationContext
+            val hostActivity =
+                activity
             val hostView =
-                activity?.window?.decorView
+                hostActivity?.window?.decorView
+
+            hostActivity?.intent?.putExtra(
+                MainActivity.EXTRA_RESTORE_SETTINGS_ROOT_AFTER_THEME_CHANGE,
+                true
+            )
 
             onBeforeThemeApplied?.invoke()
             dismiss()
@@ -74,6 +82,7 @@ class ThemeBottomSheet : BottomSheetDialogFragment(R.layout.dialog_theme_selecti
                     )
                 }
 
+                hostActivity?.recreate()
                 onThemeChanged?.invoke()
             }
         }
