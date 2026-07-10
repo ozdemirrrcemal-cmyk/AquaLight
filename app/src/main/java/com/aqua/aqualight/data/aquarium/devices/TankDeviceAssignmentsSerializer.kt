b77337a4343535e92f94495e46101bf518a20e11
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-10 22:54:38 +0300

    Add tank device assignment Proto serializer

---
+++ app/src/main/java/com/aqua/aqualight/data/aquarium/devices/TankDeviceAssignmentsSerializer.kt
@@ -0,0 +1,33 @@
+package com.aqua.aqualight.data.aquarium.devices
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object TankDeviceAssignmentsSerializer : Serializer<TankDeviceAssignmentsStore> {
+
+    override val defaultValue: TankDeviceAssignmentsStore =
+        TankDeviceAssignmentsStore.getDefaultInstance()
+
+    override suspend fun readFrom(
+        input: InputStream
+    ): TankDeviceAssignmentsStore {
+        return try {
+            TankDeviceAssignmentsStore.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException(
+                "Cannot read tank-device assignments.",
+                exception
+            )
+        }
+    }
+
+    override suspend fun writeTo(
+        t: TankDeviceAssignmentsStore,
+        output: OutputStream
+    ) {
+        t.writeTo(output)
+    }
+}
