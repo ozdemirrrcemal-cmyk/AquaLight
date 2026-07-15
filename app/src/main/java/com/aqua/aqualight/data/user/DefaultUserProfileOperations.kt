36bfd1160e32fbcd65c84bb1ac8ff28a41d3feaa
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-15 14:45:13 +0300

    Wire complete user profile operations

--- app/src/main/java/com/aqua/aqualight/data/user/DefaultUserProfileOperations.kt
+++ app/src/main/java/com/aqua/aqualight/data/user/DefaultUserProfileOperations.kt
@@ -1,5 +1,6 @@
 package com.aqua.aqualight.data.user
 
+import com.aqua.aqualight.application.user.UserAddressInput
 import com.aqua.aqualight.application.user.UserProfileOperations
 import com.aqua.aqualight.application.user.UserProfileSnapshot
 import kotlinx.coroutines.flow.Flow
@@ -14,11 +15,35 @@ class DefaultUserProfileOperations(
             UserProfileSnapshot(
                 username = prefs.username,
                 email = prefs.email,
-                profilePhotoUrl = prefs.profilePhotoUrl
+                profilePhotoUrl = prefs.profilePhotoUrl,
+                fullName = prefs.fullName,
+                firstName = prefs.firstName,
+                lastName = prefs.lastName,
+                city = prefs.city,
+                addressLine = prefs.addressLine,
+                postCode = prefs.postCode,
+                phoneNumber = prefs.phoneNumber,
+                country = prefs.country
             )
         }
 
     override suspend fun updateProfilePhoto(photoUri: String) {
         preferences.updateProfilePhoto(photoUri)
+    }
+
+    override suspend fun updateUsername(username: String) {
+        preferences.updateUsername(username)
+    }
+
+    override suspend fun saveAddress(address: UserAddressInput) {
+        preferences.saveAddress(
+            firstName = address.firstName,
+            lastName = address.lastName,
+            city = address.city,
+            addressLine = address.addressLine,
+            postCode = address.postCode,
+            phoneNumber = address.phoneNumber,
+            country = address.country
+        )
     }
 }
