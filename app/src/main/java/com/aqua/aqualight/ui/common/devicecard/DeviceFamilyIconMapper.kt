913c96ec79387d6617c613e535fe294ceacfb241
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-15 23:07:06 +0300

    refactor: support application device families

--- app/src/main/java/com/aqua/aqualight/ui/common/devicecard/DeviceFamilyIconMapper.kt
+++ app/src/main/java/com/aqua/aqualight/ui/common/devicecard/DeviceFamilyIconMapper.kt
@@ -2,6 +2,7 @@ package com.aqua.aqualight.ui.common.devicecard
 
 import androidx.annotation.DrawableRes
 import com.aqua.aqualight.R
+import com.aqua.aqualight.application.devices.OwnerDeviceFamily
 import com.aqua.aqualight.data.devices.model.DeviceFamily
 
 object DeviceFamilyIconMapper {
@@ -14,6 +15,17 @@ object DeviceFamilyIconMapper {
             DeviceFamily.DOSING -> R.drawable.img_dosing_pump_4ch
             DeviceFamily.COOLING -> R.drawable.ic_device_cooling
             DeviceFamily.UNKNOWN -> R.drawable.ic_device_aqua_ster
+        }
+    }
+
+    @DrawableRes
+    fun iconFor(family: OwnerDeviceFamily): Int {
+        return when (family) {
+            OwnerDeviceFamily.LIGHT -> R.drawable.ic_device_light
+            OwnerDeviceFamily.TIMER -> R.drawable.ic_device_timer
+            OwnerDeviceFamily.DOSING -> R.drawable.img_dosing_pump_4ch
+            OwnerDeviceFamily.COOLING -> R.drawable.ic_device_cooling
+            OwnerDeviceFamily.UNKNOWN -> R.drawable.ic_device_aqua_ster
         }
     }
 }
