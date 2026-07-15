791681dd6777f4fba30b2b9e9cb99f5dd8353926
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-15 14:44:54 +0300

    Expand user profile application boundary

--- app/src/main/java/com/aqua/aqualight/application/user/UserProfileOperations.kt
+++ app/src/main/java/com/aqua/aqualight/application/user/UserProfileOperations.kt
@@ -7,10 +7,32 @@ interface UserProfileOperations {
     val profile: Flow<UserProfileSnapshot>
 
     suspend fun updateProfilePhoto(photoUri: String)
+
+    suspend fun updateUsername(username: String)
+
+    suspend fun saveAddress(address: UserAddressInput)
 }
 
 data class UserProfileSnapshot(
-    val username: String,
-    val email: String,
-    val profilePhotoUrl: String
+    val username: String = "",
+    val email: String = "",
+    val profilePhotoUrl: String = "",
+    val fullName: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val city: String = "",
+    val addressLine: String = "",
+    val postCode: String = "",
+    val phoneNumber: String = "",
+    val country: String = ""
+)
+
+data class UserAddressInput(
+    val firstName: String,
+    val lastName: String,
+    val city: String,
+    val addressLine: String,
+    val postCode: String,
+    val phoneNumber: String,
+    val country: String
 )
