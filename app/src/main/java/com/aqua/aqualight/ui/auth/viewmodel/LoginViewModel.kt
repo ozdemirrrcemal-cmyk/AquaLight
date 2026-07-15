fbb273d394d34c468229ca29a6e0959972495896
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-15 09:47:23 +0300

    Depend LoginViewModel on AuthOperations

--- app/src/main/java/com/aqua/aqualight/ui/auth/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/aqua/aqualight/ui/auth/viewmodel/LoginViewModel.kt
@@ -3,8 +3,8 @@ package com.aqua.aqualight.ui.auth.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aqua.aqualight.R
+import com.aqua.aqualight.application.auth.AuthOperations
 import com.aqua.aqualight.data.auth.AuthErrorMapper
-import com.aqua.aqualight.data.auth.AuthRepository
 import com.aqua.aqualight.data.auth.AuthUiText
 import com.aqua.aqualight.ui.auth.state.AuthActionState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,7 +13,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
-    private val repository: AuthRepository
+    private val authOperations: AuthOperations
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<AuthActionState>(
@@ -37,7 +37,7 @@ class LoginViewModel(
             _state.value = AuthActionState.Loading
 
             runCatching {
-                repository.signInWithGoogleToken(
+                authOperations.signInWithGoogleToken(
                     idToken = idToken
                 )
             }.onSuccess {
