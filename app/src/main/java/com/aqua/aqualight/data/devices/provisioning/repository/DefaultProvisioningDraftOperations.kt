6f55d9663ca272694108dd04f6c07ee6a7317437
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-16 14:41:30 +0300

    Use encrypted provisioning session storage in draft adapter

--- app/src/main/java/com/aqua/aqualight/data/devices/provisioning/repository/DefaultProvisioningDraftOperations.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/provisioning/repository/DefaultProvisioningDraftOperations.kt
@@ -1,13 +1,22 @@
 package com.aqua.aqualight.data.devices.provisioning.repository
 
+import android.content.Context
 import com.aqua.aqualight.application.devices.provisioning.ProvisioningDraftOperations
 import com.aqua.aqualight.application.devices.provisioning.ProvisioningDraftRequest
 import com.aqua.aqualight.application.devices.provisioning.ProvisioningDraftSession
 import com.aqua.aqualight.data.devices.provisioning.model.AqlWifiCredentials
 import com.aqua.aqualight.data.devices.provisioning.store.AqlProvisioningBleAddressCache
 import com.aqua.aqualight.data.devices.provisioning.store.AqlProvisioningDraftStore
+import com.aqua.aqualight.data.devices.provisioning.store.ProvisioningDraftStorage
 
-class DefaultProvisioningDraftOperations : ProvisioningDraftOperations {
+class DefaultProvisioningDraftOperations internal constructor(
+    private val draftStore: ProvisioningDraftStorage,
+    private val cachedBleAddress: (String) -> String = AqlProvisioningBleAddressCache::get
+) : ProvisioningDraftOperations {
+
+    constructor(context: Context) : this(
+        draftStore = AqlProvisioningDraftStore(context.applicationContext)
+    )
 
     override fun createDraft(
         request: ProvisioningDraftRequest
@@ -18,10 +27,10 @@ class DefaultProvisioningDraftOperations : ProvisioningDraftOperations {
             timezone = request.timezone,
             utcOffsetMinutes = request.utcOffsetMinutes
         )
-        val draft = AqlProvisioningDraftStore.create(
+        val draft = draftStore.create(
             candidateId = request.candidateId,
             bleAddress = request.bleAddress.ifBlank {
-                AqlProvisioningBleAddressCache.get(request.bleName)
+                cachedBleAddress(request.bleName)
             },
             bleName = request.bleName,
             claimCode = request.claimCode,
@@ -31,8 +40,6 @@ class DefaultProvisioningDraftOperations : ProvisioningDraftOperations {
             deviceModel = request.deviceModel,
             wifiCredentials = credentials
         )
-        ProvisioningDraftSession(
-            sessionId = draft.sessionId
-        )
+        ProvisioningDraftSession(sessionId = draft.sessionId)
     }
 }
