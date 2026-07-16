b161b221d62a1e09fa60c3a3025a8d8f399ba7dc
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-16 19:16:38 +0300

    Point assignment guard at owner factory

--- tools/tank_device_assignment_boundary_guard.py
+++ tools/tank_device_assignment_boundary_guard.py
@@ -13,7 +13,7 @@ MAPPING = SOURCE / "data/devices/DeviceApplicationMapping.kt"
 DETAIL_VM = SOURCE / "ui/tabs/aquarium/detail/devices/TankDetailDevicesViewModel.kt"
 SELECT_VM = SOURCE / "ui/tabs/aquarium/detail/devices/select/TankDeviceSelectViewModel.kt"
 CARD_MAPPER = SOURCE / "ui/common/devicecard/DeviceCompactSnapshotMapper.kt"
-FACTORY = SOURCE / "composition/AquaViewModelFactory.kt"
+FACTORY = SOURCE / "composition/OwnerViewModelFactory.kt"
 SMOKE_FACTORY = ROOT / "app/src/releaseSmoke/java/com/aqua/aqualight/smoke/Stage3SmokeAppContainer.kt"
 TEST = ROOT / "app/src/test/java/com/aqua/aqualight/ui/tabs/aquarium/detail/devices/TankDeviceAssignmentViewModelBoundaryTest.kt"
 
