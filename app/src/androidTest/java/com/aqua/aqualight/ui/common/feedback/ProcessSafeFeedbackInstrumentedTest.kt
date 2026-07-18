4b0d12f8422b319d03a76b52d6d0e269faa46feb
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-18 11:23:32 +0300

    Cover tank editor recreation contract

--- app/src/androidTest/java/com/aqua/aqualight/ui/common/feedback/ProcessSafeFeedbackInstrumentedTest.kt
+++ app/src/androidTest/java/com/aqua/aqualight/ui/common/feedback/ProcessSafeFeedbackInstrumentedTest.kt
@@ -1,10 +1,12 @@
 package com.aqua.aqualight.ui.common.feedback
 
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.aqua.aqualight.ui.common.bottomsheet.CareTaskTypeBottomSheetFragment
 import com.aqua.aqualight.ui.common.bottomsheet.CountryPickerBottomSheet
 import com.aqua.aqualight.ui.common.bottomsheet.PhotoSourceBottomSheet
+import com.aqua.aqualight.ui.common.bottomsheet.TankSettingsEditorBottomSheet
 import com.aqua.aqualight.ui.common.bottomsheet.ThemeBottomSheet
 import com.aqua.aqualight.ui.common.permission.CapabilityPermissionBottomSheet
 import org.junit.Assert.assertEquals
@@ -24,6 +26,7 @@ class ProcessSafeFeedbackInstrumentedTest {
             CountryPickerBottomSheet::class.java,
             CareTaskTypeBottomSheetFragment::class.java,
             CapabilityPermissionBottomSheet::class.java,
+            TankSettingsEditorBottomSheet::class.java,
             FeedbackBottomSheet::class.java
         )
 
@@ -44,6 +47,34 @@ class ProcessSafeFeedbackInstrumentedTest {
             actionId = "device-123"
         )
         val recreated = FeedbackBottomSheet::class.java
+            .getDeclaredConstructor()
+            .newInstance()
+            .apply {
+                arguments = Bundle(original.requireArguments())
+            }
+
+        assertEquals(original.requireArguments(), recreated.requireArguments())
+    }
+
+    @Test
+    fun tankEditorArgumentsCanRecreateTheSameModeAndValues() {
+        val original = TankSettingsEditorBottomSheet().apply {
+            arguments = bundleOf(
+                "arg_mode" to TankSettingsEditorBottomSheet.Mode.SIZE.name,
+                "arg_title" to "Tank size",
+                "arg_current_text" to "",
+                "arg_validation_message" to "Invalid size",
+                "arg_current_millis" to Long.MIN_VALUE,
+                "arg_min_year" to 2000,
+                "arg_max_year" to 2100,
+                "arg_locale_tag" to "en-US",
+                "arg_width_cm" to 60,
+                "arg_length_cm" to 40,
+                "arg_height_cm" to 40,
+                "arg_current_unit" to "cm"
+            )
+        }
+        val recreated = TankSettingsEditorBottomSheet::class.java
             .getDeclaredConstructor()
             .newInstance()
             .apply {
