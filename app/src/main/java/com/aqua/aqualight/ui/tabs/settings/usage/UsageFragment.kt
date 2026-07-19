5571d3bd6f2d3d97a0901cec6aacd79f3f018a83
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-19 14:11:58 +0300

    cleanup: remove unused usage import

--- app/src/main/java/com/aqua/aqualight/ui/tabs/settings/usage/UsageFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/settings/usage/UsageFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import coil3.load
 import com.aqua.aqualight.R
 import com.aqua.aqualight.application.user.UsageAnalyticsSnapshot
 import com.aqua.aqualight.composition.requireAppContainer
