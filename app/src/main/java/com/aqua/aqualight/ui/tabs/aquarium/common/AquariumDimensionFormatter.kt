6e0bf529d694e6732d098cc4d818b3cde8a46591
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-19 11:19:59 +0300

    Localize aquarium numbers

--- app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/common/AquariumDimensionFormatter.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/common/AquariumDimensionFormatter.kt
@@ -3,18 +3,12 @@ package com.aqua.aqualight.ui.tabs.aquarium.common
 import android.content.Context
 import androidx.annotation.StringRes
 import com.aqua.aqualight.R
-import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
+import com.aqua.aqualight.localization.LocaleFormatters
 import java.util.Locale
 import kotlin.math.roundToInt
 
 object AquariumDimensionFormatter {
     private const val GALLON_PER_LITER = 0.264172
-
-    private val preciseFormatter = DecimalFormat(
-        "#0.##",
-        DecimalFormatSymbols(Locale.US)
-    )
 
     fun sizeTitle(
         context: Context,
@@ -35,26 +29,23 @@ object AquariumDimensionFormatter {
         sizeUnit: String,
         @StringRes separatorRes: Int = R.string.aquarium_dimension_separator_spaced_multiply
     ): String {
+        val locale = context.currentLocale()
         val separator = context.getString(separatorRes)
         return if (sizeUnit.equals("in", ignoreCase = true)) {
-            val widthIn = widthCm / 2.54
-            val lengthIn = lengthCm / 2.54
-            val heightIn = heightCm / 2.54
-
             context.getString(
                 R.string.aquarium_dimension_format,
-                preciseFormatter.format(widthIn),
+                LocaleFormatters.formatNumber(widthCm / 2.54, locale),
                 separator,
-                preciseFormatter.format(lengthIn),
-                preciseFormatter.format(heightIn)
+                LocaleFormatters.formatNumber(lengthCm / 2.54, locale),
+                LocaleFormatters.formatNumber(heightCm / 2.54, locale)
             )
         } else {
             context.getString(
                 R.string.aquarium_dimension_format,
-                widthCm.toString(),
+                LocaleFormatters.formatInteger(widthCm.toLong(), locale),
                 separator,
-                lengthCm.toString(),
-                heightCm.toString()
+                LocaleFormatters.formatInteger(lengthCm.toLong(), locale),
+                LocaleFormatters.formatInteger(heightCm.toLong(), locale)
             )
         }
     }
@@ -67,19 +58,33 @@ object AquariumDimensionFormatter {
         volumeUnit: String,
         rounded: Boolean = false
     ): String {
+        val locale = context.currentLocale()
         val liters = (widthCm * lengthCm * heightCm) / 1000.0
 
         return if (volumeUnit.equals("gal", ignoreCase = true)) {
             val gallons = liters * GALLON_PER_LITER
             context.getString(
                 R.string.aquarium_volume_gallon_format,
-                if (rounded) gallons.roundToInt().toString() else preciseFormatter.format(gallons)
+                if (rounded) {
+                    LocaleFormatters.formatInteger(gallons.roundToInt().toLong(), locale)
+                } else {
+                    LocaleFormatters.formatNumber(gallons, locale)
+                }
             )
         } else {
             context.getString(
                 R.string.aquarium_volume_liter_format,
-                if (rounded) liters.roundToInt().toString() else preciseFormatter.format(liters)
+                if (rounded) {
+                    LocaleFormatters.formatInteger(liters.roundToInt().toLong(), locale)
+                } else {
+                    LocaleFormatters.formatNumber(liters, locale)
+                }
             )
         }
+    }
+
+    private fun Context.currentLocale(): Locale {
+        val locales = resources.configuration.locales
+        return if (locales.isEmpty) Locale.getDefault() else locales[0]
     }
 }
