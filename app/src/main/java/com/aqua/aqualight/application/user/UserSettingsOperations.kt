d212c5a98c061abc5ead6f026c43cf6e7f178c5a
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-19 09:36:16 +0300

    refactor(stage11): centralize default locale constant

--- app/src/main/java/com/aqua/aqualight/application/user/UserSettingsOperations.kt
+++ app/src/main/java/com/aqua/aqualight/application/user/UserSettingsOperations.kt
@@ -1,5 +1,6 @@
 package com.aqua.aqualight.application.user
 
+import com.aqua.aqualight.localization.SupportedLocaleRegistry
 import kotlinx.coroutines.flow.Flow
 
 /** Application-facing non-notification settings boundary. */
@@ -14,7 +15,7 @@ interface UserSettingsOperations {
     suspend fun updateAutoUpdateEnabled(enabled: Boolean)
 
     companion object {
-        const val DEFAULT_LANGUAGE_CODE = "en"
+        const val DEFAULT_LANGUAGE_CODE = SupportedLocaleRegistry.DEFAULT_LANGUAGE_TAG
     }
 }
 
