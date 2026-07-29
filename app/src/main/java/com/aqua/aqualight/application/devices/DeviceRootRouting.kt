98a1b3e85e0a276b198174651be214d2e55f1b16
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 00:45:46 +0300

    Split root routing by device family

--- app/src/main/java/com/aqua/aqualight/application/devices/DeviceRootRouting.kt
+++ app/src/main/java/com/aqua/aqualight/application/devices/DeviceRootRouting.kt
@@ -29,49 +29,46 @@ object DeviceRootRouteResolver {
     fun resolve(
         family: OwnerDeviceFamily,
         feature: DeviceRootMenuFeature
-    ): DeviceRootRoute? = when (feature) {
-        DeviceRootMenuFeature.LIGHT_MANUAL -> family.onlyLight(DeviceRootRoute.LIGHT_MANUAL)
-        DeviceRootMenuFeature.LIGHT_QUICK_SETUP ->
-            family.onlyLight(DeviceRootRoute.LIGHT_QUICK_SETUP)
-        DeviceRootMenuFeature.LIGHT_PROGRAMS -> family.onlyLight(DeviceRootRoute.LIGHT_PROGRAMS)
-        DeviceRootMenuFeature.LIGHT_PRESETS -> family.onlyLight(DeviceRootRoute.LIGHT_PRESETS)
-        DeviceRootMenuFeature.LIGHT_SIMULATION ->
-            family.onlyLight(DeviceRootRoute.LIGHT_SIMULATION)
-        DeviceRootMenuFeature.DOSING_CHANNELS ->
-            family.onlyDosing(DeviceRootRoute.DOSING_CHANNELS)
-        DeviceRootMenuFeature.DOSING_CALIBRATION ->
-            family.onlyDosing(DeviceRootRoute.DOSING_CALIBRATION)
-        DeviceRootMenuFeature.DOSING_SCHEDULES ->
-            family.onlyDosing(DeviceRootRoute.DOSING_SCHEDULES)
-        DeviceRootMenuFeature.TIMER_CHANNELS ->
-            family.onlyTimer(DeviceRootRoute.TIMER_CHANNELS)
-        DeviceRootMenuFeature.TIMER_SCHEDULES ->
-            family.onlyTimer(DeviceRootRoute.TIMER_SCHEDULES)
-        DeviceRootMenuFeature.COOLING_FANS -> when (family) {
-            OwnerDeviceFamily.LIGHT -> DeviceRootRoute.LIGHT_FAN_CONTROL
-            OwnerDeviceFamily.COOLING -> DeviceRootRoute.COOLING_CONTROL
-            else -> null
-        }
-        DeviceRootMenuFeature.COOLING_TEMPERATURE -> when (family) {
-            OwnerDeviceFamily.LIGHT -> DeviceRootRoute.LIGHT_TEMPERATURE_PROTECTION
-            OwnerDeviceFamily.COOLING -> DeviceRootRoute.COOLING_TEMPERATURE
-            else -> null
-        }
-        DeviceRootMenuFeature.DEVICE_SETTINGS -> when (family) {
-            OwnerDeviceFamily.LIGHT,
-            OwnerDeviceFamily.TIMER,
-            OwnerDeviceFamily.DOSING,
-            OwnerDeviceFamily.COOLING -> DeviceRootRoute.DEVICE_SETTINGS
-            OwnerDeviceFamily.UNKNOWN -> null
-        }
+    ): DeviceRootRoute? = when (family) {
+        OwnerDeviceFamily.LIGHT -> resolveLight(feature)
+        OwnerDeviceFamily.TIMER -> resolveTimer(feature)
+        OwnerDeviceFamily.DOSING -> resolveDosing(feature)
+        OwnerDeviceFamily.COOLING -> resolveCooling(feature)
+        OwnerDeviceFamily.UNKNOWN -> null
     }
 
-    private fun OwnerDeviceFamily.onlyLight(route: DeviceRootRoute): DeviceRootRoute? =
-        route.takeIf { this == OwnerDeviceFamily.LIGHT }
+    private fun resolveLight(feature: DeviceRootMenuFeature): DeviceRootRoute? = when (feature) {
+        DeviceRootMenuFeature.LIGHT_MANUAL -> DeviceRootRoute.LIGHT_MANUAL
+        DeviceRootMenuFeature.LIGHT_QUICK_SETUP -> DeviceRootRoute.LIGHT_QUICK_SETUP
+        DeviceRootMenuFeature.LIGHT_PROGRAMS -> DeviceRootRoute.LIGHT_PROGRAMS
+        DeviceRootMenuFeature.LIGHT_PRESETS -> DeviceRootRoute.LIGHT_PRESETS
+        DeviceRootMenuFeature.LIGHT_SIMULATION -> DeviceRootRoute.LIGHT_SIMULATION
+        DeviceRootMenuFeature.COOLING_FANS -> DeviceRootRoute.LIGHT_FAN_CONTROL
+        DeviceRootMenuFeature.COOLING_TEMPERATURE ->
+            DeviceRootRoute.LIGHT_TEMPERATURE_PROTECTION
+        DeviceRootMenuFeature.DEVICE_SETTINGS -> DeviceRootRoute.DEVICE_SETTINGS
+        else -> null
+    }
 
-    private fun OwnerDeviceFamily.onlyTimer(route: DeviceRootRoute): DeviceRootRoute? =
-        route.takeIf { this == OwnerDeviceFamily.TIMER }
+    private fun resolveTimer(feature: DeviceRootMenuFeature): DeviceRootRoute? = when (feature) {
+        DeviceRootMenuFeature.TIMER_CHANNELS -> DeviceRootRoute.TIMER_CHANNELS
+        DeviceRootMenuFeature.TIMER_SCHEDULES -> DeviceRootRoute.TIMER_SCHEDULES
+        DeviceRootMenuFeature.DEVICE_SETTINGS -> DeviceRootRoute.DEVICE_SETTINGS
+        else -> null
+    }
 
-    private fun OwnerDeviceFamily.onlyDosing(route: DeviceRootRoute): DeviceRootRoute? =
-        route.takeIf { this == OwnerDeviceFamily.DOSING }
+    private fun resolveDosing(feature: DeviceRootMenuFeature): DeviceRootRoute? = when (feature) {
+        DeviceRootMenuFeature.DOSING_CHANNELS -> DeviceRootRoute.DOSING_CHANNELS
+        DeviceRootMenuFeature.DOSING_CALIBRATION -> DeviceRootRoute.DOSING_CALIBRATION
+        DeviceRootMenuFeature.DOSING_SCHEDULES -> DeviceRootRoute.DOSING_SCHEDULES
+        DeviceRootMenuFeature.DEVICE_SETTINGS -> DeviceRootRoute.DEVICE_SETTINGS
+        else -> null
+    }
+
+    private fun resolveCooling(feature: DeviceRootMenuFeature): DeviceRootRoute? = when (feature) {
+        DeviceRootMenuFeature.COOLING_FANS -> DeviceRootRoute.COOLING_CONTROL
+        DeviceRootMenuFeature.COOLING_TEMPERATURE -> DeviceRootRoute.COOLING_TEMPERATURE
+        DeviceRootMenuFeature.DEVICE_SETTINGS -> DeviceRootRoute.DEVICE_SETTINGS
+        else -> null
+    }
 }
