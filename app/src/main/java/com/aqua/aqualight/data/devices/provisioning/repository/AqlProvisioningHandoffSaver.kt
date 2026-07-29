d5881392e9500a8e40840ca54d5157a922039da5
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 01:37:42 +0300

    Document atomic provisioning transaction complexity

--- app/src/main/java/com/aqua/aqualight/data/devices/provisioning/repository/AqlProvisioningHandoffSaver.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/provisioning/repository/AqlProvisioningHandoffSaver.kt
@@ -23,6 +23,7 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 
+@Suppress("LongMethod")
 class AqlProvisioningHandoffSaver(
     context: Context
 ) {
