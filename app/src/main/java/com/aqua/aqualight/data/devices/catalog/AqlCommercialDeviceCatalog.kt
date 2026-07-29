ac851c2dc1f2fdff872c543eb7093e16c9ed8607
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 00:49:36 +0300

    Separate runtime module contract from catalog validator

--- app/src/main/java/com/aqua/aqualight/data/devices/catalog/AqlCommercialDeviceCatalog.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/catalog/AqlCommercialDeviceCatalog.kt
@@ -187,24 +187,6 @@ private fun DeviceSnapshot.toReportedCatalogProduct(): Result<ReportedCatalogPro
     )
 }
 
-private fun AqlCommercialCatalogProduct.expectedRuntimeModules(): DeviceRuntimeModules {
-    val profileCapabilities = profile.capabilities
-    val standaloneTimer = family == DeviceFamily.TIMER && profileCapabilities.standaloneTimer
-    val dosingProduct = family == DeviceFamily.DOSING && profileCapabilities.dosing
-    return DeviceRuntimeModules(
-        light = family == DeviceFamily.LIGHT && profileCapabilities.light,
-        cooling = profileCapabilities.cooling,
-        temperature = profileCapabilities.temperature,
-        timerApi = standaloneTimer,
-        timerEngine = standaloneTimer || dosingProduct,
-        dosing = dosingProduct,
-        network = true,
-        discovery = true,
-        firmware = true,
-        system = true
-    )
-}
-
 private fun DeviceProduct.toCompatibilityIdentity(): DeviceCompatibilityIdentity =
     DeviceCompatibilityIdentity(
         productKey = DeviceProductKey(productKey),
