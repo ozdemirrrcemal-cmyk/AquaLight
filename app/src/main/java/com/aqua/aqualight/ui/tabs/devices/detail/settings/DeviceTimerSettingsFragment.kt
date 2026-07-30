be6693a79d66beb8db3830b58ab8af6cd2ba2413
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 22:08:25 +0300

    Use the shared copy in Timer Settings

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceTimerSettingsFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceTimerSettingsFragment.kt
@@ -12,18 +12,5 @@ class DeviceTimerSettingsFragment : DeviceFamilySettingsFragment(TIMER_SETTINGS_
 }
 
 private val TIMER_SETTINGS_COPY = DeviceFamilySettingsCopy(
-    screenTitleRes = R.string.device_timer_settings_title,
-    information = DeviceInformationSettingsCopy(
-        sectionTitleRes = R.string.device_timer_settings_device_information_section,
-        deviceNameLabelRes = R.string.device_timer_settings_device_name_label,
-        editDeviceNameDescriptionRes = R.string.device_timer_settings_edit_device_name_description,
-        serialNumberLabelRes = R.string.device_timer_settings_serial_number_label,
-        hardwareRevisionLabelRes = R.string.device_timer_settings_hardware_revision_label
-    ),
-    software = DeviceSoftwareSettingsCopy(
-        sectionTitleRes = R.string.device_timer_settings_software_section,
-        firmwareVersionLabelRes = R.string.device_timer_settings_firmware_version_label,
-        checkForUpdatesActionRes = R.string.device_timer_settings_check_updates_action
-    ),
-    unavailableValueRes = R.string.device_timer_settings_unavailable_value
+    screenTitleRes = R.string.device_timer_settings_title
 )
