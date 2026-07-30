f46c805a5e49ee27efd17ee6db7d4ab0dc2eb3a5
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 19:03:46 +0300

    Configure Cooling Settings copy

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceCoolingSettingsFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceCoolingSettingsFragment.kt
@@ -3,11 +3,27 @@ package com.aqua.aqualight.ui.tabs.devices.detail.settings
 import androidx.navigation.fragment.navArgs
 import com.aqua.aqualight.R
 
-class DeviceCoolingSettingsFragment :
-    DeviceFamilySettingsFragment(R.string.device_cooling_settings_title) {
+class DeviceCoolingSettingsFragment : DeviceFamilySettingsFragment(COOLING_SETTINGS_COPY) {
 
     private val args: DeviceCoolingSettingsFragmentArgs by navArgs()
 
     override val deviceUid: String
         get() = args.deviceUid
 }
+
+private val COOLING_SETTINGS_COPY = DeviceFamilySettingsCopy(
+    screenTitleRes = R.string.device_cooling_settings_title,
+    information = DeviceInformationSettingsCopy(
+        sectionTitleRes = R.string.device_cooling_settings_device_information_section,
+        deviceNameLabelRes = R.string.device_cooling_settings_device_name_label,
+        editDeviceNameDescriptionRes = R.string.device_cooling_settings_edit_device_name_description,
+        serialNumberLabelRes = R.string.device_cooling_settings_serial_number_label,
+        hardwareRevisionLabelRes = R.string.device_cooling_settings_hardware_revision_label
+    ),
+    software = DeviceSoftwareSettingsCopy(
+        sectionTitleRes = R.string.device_cooling_settings_software_section,
+        firmwareVersionLabelRes = R.string.device_cooling_settings_firmware_version_label,
+        checkForUpdatesActionRes = R.string.device_cooling_settings_check_updates_action
+    ),
+    unavailableValueRes = R.string.device_cooling_settings_unavailable_value
+)
