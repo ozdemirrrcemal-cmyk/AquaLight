280bacab4a88ed59ef7dc0189b5e5035a8f6f69b
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 22:08:34 +0300

    Use the shared copy in Cooling Settings

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceCoolingSettingsFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceCoolingSettingsFragment.kt
@@ -12,18 +12,5 @@ class DeviceCoolingSettingsFragment : DeviceFamilySettingsFragment(COOLING_SETTI
 }
 
 private val COOLING_SETTINGS_COPY = DeviceFamilySettingsCopy(
-    screenTitleRes = R.string.device_cooling_settings_title,
-    information = DeviceInformationSettingsCopy(
-        sectionTitleRes = R.string.device_cooling_settings_device_information_section,
-        deviceNameLabelRes = R.string.device_cooling_settings_device_name_label,
-        editDeviceNameDescriptionRes = R.string.device_cooling_settings_edit_device_name_description,
-        serialNumberLabelRes = R.string.device_cooling_settings_serial_number_label,
-        hardwareRevisionLabelRes = R.string.device_cooling_settings_hardware_revision_label
-    ),
-    software = DeviceSoftwareSettingsCopy(
-        sectionTitleRes = R.string.device_cooling_settings_software_section,
-        firmwareVersionLabelRes = R.string.device_cooling_settings_firmware_version_label,
-        checkForUpdatesActionRes = R.string.device_cooling_settings_check_updates_action
-    ),
-    unavailableValueRes = R.string.device_cooling_settings_unavailable_value
+    screenTitleRes = R.string.device_cooling_settings_title
 )
