9f9f3fe5c95a4eb32f74fa6ea86d20088efb204f
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-30 19:03:17 +0300

    Configure Dosing Settings copy

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceDosingSettingsFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/settings/DeviceDosingSettingsFragment.kt
@@ -3,11 +3,27 @@ package com.aqua.aqualight.ui.tabs.devices.detail.settings
 import androidx.navigation.fragment.navArgs
 import com.aqua.aqualight.R
 
-class DeviceDosingSettingsFragment :
-    DeviceFamilySettingsFragment(R.string.device_dosing_settings_title) {
+class DeviceDosingSettingsFragment : DeviceFamilySettingsFragment(DOSING_SETTINGS_COPY) {
 
     private val args: DeviceDosingSettingsFragmentArgs by navArgs()
 
     override val deviceUid: String
         get() = args.deviceUid
 }
+
+private val DOSING_SETTINGS_COPY = DeviceFamilySettingsCopy(
+    screenTitleRes = R.string.device_dosing_settings_title,
+    information = DeviceInformationSettingsCopy(
+        sectionTitleRes = R.string.device_dosing_settings_device_information_section,
+        deviceNameLabelRes = R.string.device_dosing_settings_device_name_label,
+        editDeviceNameDescriptionRes = R.string.device_dosing_settings_edit_device_name_description,
+        serialNumberLabelRes = R.string.device_dosing_settings_serial_number_label,
+        hardwareRevisionLabelRes = R.string.device_dosing_settings_hardware_revision_label
+    ),
+    software = DeviceSoftwareSettingsCopy(
+        sectionTitleRes = R.string.device_dosing_settings_software_section,
+        firmwareVersionLabelRes = R.string.device_dosing_settings_firmware_version_label,
+        checkForUpdatesActionRes = R.string.device_dosing_settings_check_updates_action
+    ),
+    unavailableValueRes = R.string.device_dosing_settings_unavailable_value
+)
