f821e3cd582eeebe3669bac3973241b213d455cb
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-31 03:07:49 +0300

    Mirror cooling fan name contract limits

--- app/src/main/java/com/aqua/aqualight/data/devices/runtime/modules/cooling/DeviceCoolingRuntimeContract.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/runtime/modules/cooling/DeviceCoolingRuntimeContract.kt
@@ -12,6 +12,9 @@ object DeviceCoolingRuntimeContract {
         const val MODE = "mode"
         const val MIN_TEMPERATURE_C = "minTemperatureC"
         const val MAX_TEMPERATURE_C = "maxTemperatureC"
+        const val FANS = "fans"
+        const val FAN_KEY = "fanKey"
+        const val DISPLAY_NAME = "displayName"
         const val SAVE = "save"
     }
 
@@ -20,5 +23,7 @@ object DeviceCoolingRuntimeContract {
         const val HIGHEST_MIN_C = 80.0
         const val LOWEST_MAX_C = 1.0
         const val HIGHEST_MAX_C = 90.0
+        const val MAX_FANS_PER_REQUEST = 8
+        const val MAX_DISPLAY_NAME_BYTES = 32
     }
 }
