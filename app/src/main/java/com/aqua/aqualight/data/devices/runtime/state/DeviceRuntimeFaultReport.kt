0fbffe22e04051addc2066eb22aeb415660c6508
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-07-31 12:42:10 +0300

    Complete command fault report file rename

--- app/src/main/java/com/aqua/aqualight/data/devices/runtime/state/DeviceRuntimeFaultReport.kt
+++
@@ -1,9 +0,0 @@
-package com.aqua.aqualight.data.devices.runtime.state
-
-data class DeviceRuntimeCommandFaultReport(
-    val code: String,
-    val message: String,
-    val module: String,
-    val action: String,
-    val messageId: String = ""
-)
