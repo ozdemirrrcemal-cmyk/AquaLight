a5e25e19dd22e4cfdd6e12c4e557ed7c854249fb
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-01 19:55:49 +0300

    test(light): remove unused capability fixture import

--- app/src/test/java/com/aqua/aqualight/data/devices/runtime/modules/light/DeviceLightCapabilityGateTest.kt
+++ app/src/test/java/com/aqua/aqualight/data/devices/runtime/modules/light/DeviceLightCapabilityGateTest.kt
@@ -5,7 +5,6 @@ import com.aqua.aqualight.data.devices.runtime.core.DeviceRuntimeCommand
 import com.aqua.aqualight.data.devices.runtime.core.DeviceRuntimeCommandGateway
 import com.aqua.aqualight.data.devices.runtime.core.DeviceRuntimeCommandOutcome
 import kotlinx.coroutines.runBlocking
-import org.json.JSONObject
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
