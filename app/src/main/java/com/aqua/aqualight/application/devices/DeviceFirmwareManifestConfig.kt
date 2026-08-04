b89d5abeb345b3fad217661a598b05b01cc23422
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-04 14:22:40 +0300

    remove global OTA manifest URL configuration

--- app/src/main/java/com/aqua/aqualight/application/devices/DeviceFirmwareManifestConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.aqua.aqualight.application.devices
-
-/** Safe stable fallback used by tests and non-Android application consumers. */
-const val DEVICE_FIRMWARE_MANIFEST_URL =
-    "https://github.com/ozdemirrrcemal-cmyk/AquaLight-OTA-Releases/" +
-        "releases/latest/download/manifest-stable.json"
