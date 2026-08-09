e9af85bd1a06886573730d83c4629571de748800
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-09 22:11:54 +0300

    Fix fixture dosing navigation wiring

--- app/src/releaseSmoke/java/com/aqua/aqualight/smoke/ReleaseSmokeAppContainer.kt
+++ app/src/releaseSmoke/java/com/aqua/aqualight/smoke/ReleaseSmokeAppContainer.kt
@@ -25,6 +25,7 @@ import com.aqua.aqualight.data.care.CareTaskDataStoreManager
 import com.aqua.aqualight.data.care.DefaultMaintenanceOperations
 import com.aqua.aqualight.data.care.integrity.restoreTaskSnapshotsForIntegrity
 import com.aqua.aqualight.data.care.integrity.snapshotTasksForIntegrity
+import com.aqua.aqualight.data.devices.DefaultDeviceDosingChannelNavigationOperations
 import com.aqua.aqualight.data.devices.DefaultDeviceRootOperations
 import com.aqua.aqualight.data.devices.DefaultDeviceStatusOperations
 import com.aqua.aqualight.data.devices.DefaultOwnerDevicesOperations
@@ -239,7 +240,11 @@ private class ReleaseSmokeViewModelFactory(
             DeviceTimerRootViewModel(DefaultDeviceRootOperations(devicesRepository))
 
         modelClass.isAssignableFrom(DeviceDosingRootViewModel::class.java) ->
-            DeviceDosingRootViewModel(DefaultDeviceRootOperations(devicesRepository))
+            DeviceDosingRootViewModel(
+                operations = DefaultDeviceRootOperations(devicesRepository),
+                channelNavigationOperations =
+                    DefaultDeviceDosingChannelNavigationOperations(devicesRepository)
+            )
 
         modelClass.isAssignableFrom(DeviceRootOverviewViewModel::class.java) ->
             DeviceRootOverviewViewModel(DefaultDeviceRootOperations(devicesRepository))
