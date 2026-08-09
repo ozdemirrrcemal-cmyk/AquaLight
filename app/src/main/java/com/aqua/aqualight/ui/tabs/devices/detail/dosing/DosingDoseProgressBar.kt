95437a3d2f28152d524bf590e7283c65fd09ea3b
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-09 13:00:46 +0300

    Use standard fill modifier for dose rail

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/DosingDoseProgressBar.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/DosingDoseProgressBar.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
@@ -70,7 +70,7 @@ internal fun DosingDoseProgressBar(
                 .semantics { contentDescription = description },
             contentAlignment = Alignment.Center
         ) {
-            Canvas(modifier = Modifier.matchParentSize()) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
                 val radius = size.height / 2f
                 drawRoundRect(
                     color = colors.mediaSurface,
