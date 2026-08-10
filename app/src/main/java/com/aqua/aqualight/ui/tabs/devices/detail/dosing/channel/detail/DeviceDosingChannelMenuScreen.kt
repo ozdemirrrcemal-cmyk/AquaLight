00e34c0db261ca8350289630d8cecfc13fb113a5
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-10 20:03:19 +0300

    Clean obsolete dosing detail imports

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/channel/detail/DeviceDosingChannelMenuScreen.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/channel/detail/DeviceDosingChannelMenuScreen.kt
@@ -13,14 +13,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.aqua.aqualight.R
 import com.aqua.aqualight.ui.common.devicemenu.AquaDeviceMenuChoiceChip
 import com.aqua.aqualight.ui.common.devicemenu.AquaDeviceMenuGeometry
