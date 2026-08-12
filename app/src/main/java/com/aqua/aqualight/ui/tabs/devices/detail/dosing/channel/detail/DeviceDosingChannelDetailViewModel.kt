93d956dbcbb0bf3a2e9aa8a9ccafc290d10b4cd1
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-13 01:19:50 +0300

    Use feature-scoped Dosing detail policy

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/channel/detail/DeviceDosingChannelDetailViewModel.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/channel/detail/DeviceDosingChannelDetailViewModel.kt
@@ -1,7 +1,7 @@
 package com.aqua.aqualight.ui.tabs.devices.detail.dosing.channel.detail
 
 import androidx.lifecycle.ViewModel
-import com.aqua.aqualight.application.devices.DeviceDosingChannelDetailDraftPolicy
+import com.aqua.aqualight.application.devices.dosing.DeviceDosingChannelDetailDraftPolicy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
