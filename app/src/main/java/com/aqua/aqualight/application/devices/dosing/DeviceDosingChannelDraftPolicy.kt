bdfd28f9b7b989e0ee6c047bf7a69d76a7806418
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-13 01:14:07 +0300

    Move Dosing draft policies into feature package

--- app/src/main/java/com/aqua/aqualight/application/devices/dosing/DeviceDosingChannelDraftPolicy.kt
+++ app/src/main/java/com/aqua/aqualight/application/devices/dosing/DeviceDosingChannelDraftPolicy.kt
@@ -1,4 +1,4 @@
-package com.aqua.aqualight.application.devices
+package com.aqua.aqualight.application.devices.dosing
 
 object DeviceDosingReservoirDraftPolicy {
     const val DEFAULT_CAPACITY_ML = 450.0
