66072042a61ba8af4568cb629dd96758d8e09d18
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-21 02:10:54 +0300

    Wait for authoritative readback on revisioned dosing edits

--- app/src/main/java/com/aqua/aqualight/data/devices/dosing/v1/DeviceDosingV1ChannelOperationsAdapter.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/dosing/v1/DeviceDosingV1ChannelOperationsAdapter.kt
@@ -82,7 +82,8 @@ internal class DeviceDosingV1ChannelOperationsAdapter(
             )
             repositoryProgramApply(uid, channelKey, revision, program)
         },
-        channel = DeviceDosingV1SavedMutationResult::channel
+        channel = DeviceDosingV1SavedMutationResult::channel,
+        reconcileBeforeReturn = expectedRevision != null
     ).toChannelResult()
 
     override suspend fun setMissedDoseRecoveryEnabled(
@@ -170,7 +171,8 @@ internal class DeviceDosingV1ChannelOperationsAdapter(
                 slotId,
                 settings.lowLevelAlertEnabled
             )
-        }
+        },
+        reconcileBeforeReturn = expectedRevision != null
     ).toChannelResult()
 
     override suspend fun setReservoirLowLevelAlertEnabled(
