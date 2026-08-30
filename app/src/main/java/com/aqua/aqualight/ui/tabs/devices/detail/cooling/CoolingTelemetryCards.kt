eee476b945c9e4e47392a370d2bca8e8cebdb9c3
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-08-30 22:37:57 +0300

    Fix Cooling telemetry dimensions import

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/cooling/CoolingTelemetryCards.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/cooling/CoolingTelemetryCards.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
@@ -16,10 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.aqua.aqualight.R
 import com.aqua.aqualight.ui.common.cooling.AquaCoolingDashboardAlpha
 import com.aqua.aqualight.ui.common.cooling.AquaCoolingDashboardCardSurface
