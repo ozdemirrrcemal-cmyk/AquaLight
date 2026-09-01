36c878ffcb5a21393368fc1d75ad7b1e4b39c7f7
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-01 09:41:22 +0300

    refactor(cooling): wire release smoke root dependencies

--- app/src/releaseSmoke/java/com/aqua/aqualight/smoke/ReleaseSmokeAppContainer.kt
+++ app/src/releaseSmoke/java/com/aqua/aqualight/smoke/ReleaseSmokeAppContainer.kt
@@ -32,6 +32,8 @@ import com.aqua.aqualight.data.care.integrity.snapshotTasksForIntegrity
 import com.aqua.aqualight.data.devices.DefaultDeviceRootOperations
 import com.aqua.aqualight.data.devices.DefaultDeviceStatusOperations
 import com.aqua.aqualight.data.devices.DefaultOwnerDevicesOperations
+import com.aqua.aqualight.data.devices.cooling.DefaultDeviceCoolingAutomaticSettingsOperations
+import com.aqua.aqualight.data.devices.cooling.DefaultDeviceCoolingTemperatureHistoryOperations
 import com.aqua.aqualight.data.devices.cooling.control.DefaultDeviceCoolingControlOperations
 import com.aqua.aqualight.data.devices.menu.DefaultDeviceMenuAccessOperations
 import com.aqua.aqualight.data.devices.provisioning.DefaultProvisioningDiscoveryOperations
@@ -238,7 +240,10 @@ private class ReleaseSmokeViewModelFactory(
         modelClass.isAssignableFrom(DeviceCoolingRootViewModel::class.java) ->
             DeviceCoolingRootViewModel(
                 operations = DefaultDeviceRootOperations(devicesRepository),
-                controlOperations = DefaultDeviceCoolingControlOperations(devicesRepository)
+                controlOperations = DefaultDeviceCoolingControlOperations(devicesRepository),
+                historyOperations = DefaultDeviceCoolingTemperatureHistoryOperations(devicesRepository),
+                automaticSettingsOperations =
+                    DefaultDeviceCoolingAutomaticSettingsOperations(devicesRepository)
             )
         modelClass.isAssignableFrom(DeviceTimerRootViewModel::class.java) ->
             DeviceTimerRootViewModel(DefaultDeviceRootOperations(devicesRepository))
