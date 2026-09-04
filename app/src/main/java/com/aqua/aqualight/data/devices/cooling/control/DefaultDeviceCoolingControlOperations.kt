b5efbd3c12006485ef075658f43cb2db9bc974bb
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-04 16:31:23 +0300

    refactor(cooling): use control disconnected boundary

--- app/src/main/java/com/aqua/aqualight/data/devices/cooling/control/DefaultDeviceCoolingControlOperations.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/cooling/control/DefaultDeviceCoolingControlOperations.kt
@@ -1,10 +1,10 @@
 package com.aqua.aqualight.data.devices.cooling.control
 
 import com.aqua.aqualight.application.devices.cooling.control.DeviceCoolingControlOperations
-import com.aqua.aqualight.data.devices.cooling.DisconnectedDeviceCoolingOperations
+import com.aqua.aqualight.data.devices.cooling.DisconnectedDeviceCoolingControlOperations
 import com.aqua.aqualight.data.devices.repository.DevicesRepository
 
 /** Composition shell with no Cooling transport until strict V1 wiring is connected. */
 internal class DefaultDeviceCoolingControlOperations(
     @Suppress("UNUSED_PARAMETER") devicesRepository: DevicesRepository
-) : DeviceCoolingControlOperations by DisconnectedDeviceCoolingOperations
+) : DeviceCoolingControlOperations by DisconnectedDeviceCoolingControlOperations
