279ab17f7d6c3d53fdd581eb9476cbe570fbc3c9
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-04 16:31:16 +0300

    refactor(cooling): use history disconnected boundary

--- app/src/main/java/com/aqua/aqualight/data/devices/cooling/DefaultDeviceCoolingTemperatureHistoryOperations.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/cooling/DefaultDeviceCoolingTemperatureHistoryOperations.kt
@@ -6,4 +6,5 @@ import com.aqua.aqualight.data.devices.repository.DevicesRepository
 /** Composition shell with no Cooling transport until strict V1 wiring is connected. */
 internal class DefaultDeviceCoolingTemperatureHistoryOperations(
     @Suppress("UNUSED_PARAMETER") devicesRepository: DevicesRepository
-) : DeviceCoolingTemperatureHistoryOperations by DisconnectedDeviceCoolingOperations
+) : DeviceCoolingTemperatureHistoryOperations by
+    DisconnectedDeviceCoolingTemperatureHistoryOperations
