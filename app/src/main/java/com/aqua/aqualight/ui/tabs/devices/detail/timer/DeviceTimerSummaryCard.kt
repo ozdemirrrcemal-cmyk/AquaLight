0fe9060dc537a38e417f7f4fef744fcd8a669149
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-08 10:13:18 +0300

    fix(timer): use plural channel summary copy

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/timer/DeviceTimerSummaryCard.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/timer/DeviceTimerSummaryCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import com.aqua.aqualight.R
@@ -57,7 +58,7 @@ internal fun DeviceTimerSummaryCard(
                     overflow = TextOverflow.Ellipsis
                 )
                 BasicText(
-                    text = stringResource(
+                    text = pluralStringResource(
                         R.string.device_timer_dashboard_active_summary,
                         control.activeChannelCount,
                         control.channels.size
