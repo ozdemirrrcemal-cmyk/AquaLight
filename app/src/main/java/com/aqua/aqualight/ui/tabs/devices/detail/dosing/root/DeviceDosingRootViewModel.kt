fe2799c31228644bc709a522c8683725d350d5a9
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-22 00:12:24 +0300

    fix(dosing): keep connection state owned by device presence

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/root/DeviceDosingRootViewModel.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/detail/dosing/root/DeviceDosingRootViewModel.kt
@@ -19,6 +19,7 @@ import com.aqua.aqualight.application.devices.dosing.DeviceDosingChannelNavigati
 import com.aqua.aqualight.application.devices.dosing.DeviceDosingChannelOperations
 import com.aqua.aqualight.application.devices.dosing.DeviceDosingChannelSnapshot
 import com.aqua.aqualight.ui.common.devicepresence.DeviceConnectionVisualState
+import com.aqua.aqualight.ui.common.devicepresence.toDeviceConnectionVisualState
 import com.aqua.aqualight.ui.common.text.AquaUiText
 import com.aqua.aqualight.ui.tabs.devices.detail.common.DeviceRootKind
 import com.aqua.aqualight.ui.tabs.devices.detail.common.DeviceRootMenuMapper
@@ -301,11 +302,7 @@ class DeviceDosingRootViewModel(
                 availability == OwnerDeviceAvailability.REACHABLE &&
                 catalogState == DeviceRootCatalogState.VALID &&
                 channelPresentation.authoritative
-        val connectionVisualState = if (contentEnabled) {
-            DeviceConnectionVisualState.ONLINE
-        } else {
-            DeviceConnectionVisualState.OFFLINE
-        }
+        val connectionVisualState = toDeviceConnectionVisualState()
         return DeviceDosingRootUiState(
             title = title,
             deviceUid = deviceUid,
