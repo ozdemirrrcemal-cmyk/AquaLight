a86a4a8da7de73cd65884319ecd316e71e41b0aa
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-21 19:32:48 +0300

    feat(provisioning): keep OTA rescue reachable after setup

--- app/src/main/java/com/aqua/aqualight/ui/tabs/devices/add/DeviceProvisioningProgressFragment.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/devices/add/DeviceProvisioningProgressFragment.kt
@@ -20,6 +20,8 @@ import com.aqua.aqualight.base.BaseActivity
 import com.aqua.aqualight.composition.requireAppContainer
 import com.aqua.aqualight.databinding.FragmentDeviceProvisioningProgressBinding
 import com.aqua.aqualight.platform.permissions.AppCapability
+import com.aqua.aqualight.ui.common.devicepresence.DeviceAccessFeedbackAction
+import com.aqua.aqualight.ui.common.devicepresence.DeviceAccessFeedbackPresenter
 import com.aqua.aqualight.ui.common.devicepresence.DeviceMenuUnavailableMessageMapper
 import com.aqua.aqualight.ui.common.header.AquaHeaderConfig
 import com.aqua.aqualight.ui.common.header.setupAquaHeader
@@ -130,10 +132,21 @@ class DeviceProvisioningProgressFragment : Fragment(R.layout.fragment_device_pro
                             openAddedDevice(event.device)
                         }
                         is DeviceProvisioningProgressEvent.ShowAddedDeviceUnavailable -> {
-                            val baseActivity = activity as? BaseActivity
-                            val navController = findNavController()
-                            navController.popBackStack(R.id.devicesFragment, false)
-                            DeviceMenuUnavailableMessageMapper.feedback(event.reason).let { feedback ->
+                            val feedback = DeviceMenuUnavailableMessageMapper.feedback(event.reason)
+                            if (
+                                feedback.action ==
+                                DeviceAccessFeedbackAction.OPEN_FIRMWARE_UPDATE
+                            ) {
+                                DeviceAccessFeedbackPresenter.show(
+                                    fragment = this@DeviceProvisioningProgressFragment,
+                                    deviceUid = event.deviceUid,
+                                    deviceTitle = event.title,
+                                    reason = event.reason
+                                )
+                            } else {
+                                val baseActivity = activity as? BaseActivity
+                                val navController = findNavController()
+                                navController.popBackStack(R.id.devicesFragment, false)
                                 baseActivity?.showDeviceAccessDialog(
                                     deviceTitle = event.title,
                                     titleRes = feedback.titleRes,
