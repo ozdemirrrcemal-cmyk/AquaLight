3a3e0b45014302455f9193fd8c8b6e79fb3ebb3c
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-22 23:10:50 +0300

    cleanup(livestock): remove stale picker locale import

--- app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/detail/livestock/TankLivestockPickerAdapter.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/detail/livestock/TankLivestockPickerAdapter.kt
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.aqua.aqualight.R
 import com.aqua.aqualight.application.aquarium.LivestockCatalogItem
 import com.aqua.aqualight.databinding.ItemLivestockCatalogBinding
-import com.aqua.aqualight.i18n.AppLanguageController
 import com.aqua.aqualight.ui.tabs.aquarium.catalog.livestock.LivestockCategories
 import com.aqua.aqualight.ui.tabs.aquarium.catalog.livestock.localizedName
 
