e28c7feb6711d10d204e24db2e1227be58d119c5
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-22 17:05:50 +0300

    feat(ota): retain snapshot failure code

--- app/src/main/java/com/aqua/aqualight/data/devices/runtime/modules/firmware/DeviceFirmwareModels.kt
+++ app/src/main/java/com/aqua/aqualight/data/devices/runtime/modules/firmware/DeviceFirmwareModels.kt
@@ -83,6 +83,7 @@ data class DeviceFirmwareOtaSnapshot(
     val targetVersion: String = "",
     val sha256Expected: String = "",
     val sha256Actual: String = "",
+    val failureCode: String = "",
     val lastError: String = "",
     val lastErrorField: String = "",
     val urlScheme: String = "",
