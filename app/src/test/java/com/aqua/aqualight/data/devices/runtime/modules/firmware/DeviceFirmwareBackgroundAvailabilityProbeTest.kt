64e7045d8b312d2dd1f920639ea8de9ea19484bb
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-22 05:17:23 +0300

    test(ota): include maintenance contract in target fixtures

--- app/src/test/java/com/aqua/aqualight/data/devices/runtime/modules/firmware/DeviceFirmwareBackgroundAvailabilityProbeTest.kt
+++ app/src/test/java/com/aqua/aqualight/data/devices/runtime/modules/firmware/DeviceFirmwareBackgroundAvailabilityProbeTest.kt
@@ -176,6 +176,7 @@ class DeviceFirmwareBackgroundAvailabilityProbeTest {
                 wsSchema = "aql.ws.v1",
                 wsProtocolVersion = 1,
                 deviceApiVersion = 1,
+                maintenanceSchema = DeviceFirmwareRuntimeContract.MAINTENANCE_SCHEMA,
                 requiredDomains = listOf("aqualight.dosing.v1"),
                 optionalDomains = emptyList()
             ),
