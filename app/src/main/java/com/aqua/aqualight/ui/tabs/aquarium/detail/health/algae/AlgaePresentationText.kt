2d947142ca84c8f06eb21801027214c93df83a5a
ozdemirrrcemal-cmyk <ozdemirrr.cemal@gmail.com>
2026-09-24 15:32:21 +0300

    Present semantic algae missing data

--- app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/detail/health/algae/AlgaePresentationText.kt
+++ app/src/main/java/com/aqua/aqualight/ui/tabs/aquarium/detail/health/algae/AlgaePresentationText.kt
@@ -90,10 +90,11 @@ object AlgaePresentationText {
 
     @StringRes
     fun missing(value: AlgaeMissingData): Int = when (value) {
-        AlgaeMissingData.LIGHT_SCHEDULE -> R.string.algae_missing_light
+        AlgaeMissingData.LIGHT_PROFILE -> R.string.algae_missing_light
         AlgaeMissingData.CO2_SCHEDULE -> R.string.algae_missing_co2
         AlgaeMissingData.WATER_ANALYSIS -> R.string.algae_missing_water
         AlgaeMissingData.MAINTENANCE_HISTORY -> R.string.algae_missing_maintenance
+        AlgaeMissingData.TEMPERATURE_CONTEXT -> R.string.algae_missing_temperature
     }
 
     @StringRes
